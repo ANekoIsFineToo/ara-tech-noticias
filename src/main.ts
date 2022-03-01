19538d361f70863ce988c5059b68586aa4771d0f
Iker Pérez Brunelli <anift@ikerperez.me>
2022-03-02 00:53:03 +0100

    feat: persist akita state into local storage

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableAkitaProdMode, persistState } from '@datorama/akita';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
+  enableAkitaProdMode()
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+// persist akita state into local storage, this keeps the session available on refresh
+const storage = persistState();
+const providers = [{ provide: 'persistStorage', useValue: storage }];
+
+platformBrowserDynamic(providers)
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
